f6ff895acd8f85b58d9a8e6216fee7d95284bef4
TallesFaria <tallesbeginsc@gmail.com>
2017-03-17 00:51:12 -0300

    Works Fine

---
+++ src/Search.js
@@ -0,0 +1,9 @@
+const Search = ({ value, onChange, children }) =>
+    <form>
+        {children}
+        <input
+            type="text"
+            value={value}
+            onChange={onChange}
+        />
+    </form>
